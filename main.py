575e867699bfa2dfc9eef49ebfd37b7a90f5a390
Stradex <stradexengine@gmail.com>
2025-07-20 21:59:12 +0000

    OK: Bridging discord messages towards IRC

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading
+import queue
 import discord
 import irc.client
 from dotenv import load_dotenv
@@ -11,8 +12,11 @@ IRC_PORT = int(os.getenv("IRC_PORT"))
 IRC_BOT_NAME = os.getenv("IRC_BOT_NAME")
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
+message_queue = queue.Queue()
 
-def irc_bot():
+def irc_bot(message_queue):
+  client = irc.client.Reactor()
+
   def on_invite(connection, event):
     channel = event.arguments[0]
     if channel == IRC_CHANNEL:
@@ -28,9 +32,20 @@ def irc_bot():
     connection.privmsg(IRC_CHANNEL, "Hello from Python IRC bot!")
 
   def on_pubmsg(connection, event):
-    print(f"[IRC] <{event.source.nick}> {event.arguments[0]}")
+    user = event.source.nick
+    text = event.arguments[0]
+    print(f"[IRC] <{user}> {text}")
+    message_queue.put(("irc_to_discord", f"<{user}> {text}"))
 
-  client = irc.client.Reactor()
+  def send_from_discord():
+    while True:
+      try:
+        msg_type, msg = message_queue.get(block=True)
+        if msg_type == "discord_to_irc":
+          client.connections[0].privmsg(IRC_CHANNEL, msg)
+      except Exception as e:
+        print("Error sending to IRC:", e)
+
   try:
     c = client.server().connect(IRC_SERVER, IRC_PORT, IRC_BOT_NAME)
   except irc.client.ServerConnectionError:
@@ -41,6 +56,8 @@ def irc_bot():
   c.add_global_handler("join", on_join)
   c.add_global_handler("invite", on_invite)
   c.add_global_handler("pubmsg", on_pubmsg)
+
+  threading.Thread(target=send_from_discord, daemon=True).start()
 
   client.process_forever()
 
@@ -57,9 +74,10 @@ async def on_message(message):
   if message.author == discord_client.user:
     return
   print(f"[DISCORD] <{message.author}> {message.content}")
+  message_queue.put(("discord_to_irc", f"<{message.author}> {message.content}"))
 
 
 if __name__ == "__main__":
-  irc_thread = threading.Thread(target=irc_bot)
+  irc_thread = threading.Thread(target=irc_bot, args=(message_queue,))
   irc_thread.start()
   discord_client.run(DISCORD_BOT_TOKEN)
