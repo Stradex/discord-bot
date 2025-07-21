ddb5cf22be1ef3297b443d84b3c2c07ba2959389
root <root@webcasino-nicolas>
2025-07-21 12:09:17 +0000

    WIP: Moving from IRC to IRC3 to support SSL SALS authentification

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import threading
 import asyncio
 import queue
 import discord
-import irc.client
-import irc.connection
 from dotenv import load_dotenv
 import os
-import ssl
+import irc3
 
 load_dotenv()
 IRC_CHANNEL = os.getenv("IRC_CHANNEL")
@@ -20,83 +18,84 @@ DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
 
 message_queue = queue.Queue()
 
+config = {
+  'nick': IRC_BOT_NAME,
+  'username': IRC_USERNAME,
+  'host': IRC_SERVER,
+  'port': IRC_PORT,
+  'ssl': True,
+  'sasl_username': IRC_USERNAME,
+  'sasl_password': IRC_PASSWORD,
+  'sasl_mechanism': 'PLAIN',
+  'includes': [
+    __name__,
+    'irc3.plugins.core',
+    'irc3.plugins.sasl',
+  ],
+  'autojoins': [],
+  'verbose': True,
+  'timeout': 60,
+}
+
+@irc3.plugin
+class IRCDiscordBot:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @irc3.event(r'^:\S+ INVITE (?P<botnick>\S+) :(?P<channel>\S+)$')
+    def on_invite(self, botnick=None, channel=None):
+        print(f"Invited to channel {channel}")
+        self.bot.join(channel)
+
+    @irc3.event(irc3.rfc.JOIN)
+    def on_join(self, mask, channel, **kw):
+        if mask.nick == self.bot.nick:
+            print(f"Joined {channel}")
+            self.bot.privmsg(channel, "Hello world")
+
+
 def irc_bot(message_queue):
-
-  client = irc.client.Reactor()
-#  client.add_global_handler("all_events", print)  # shows all events
-  ssl_context = ssl.create_default_context()
-  factory = irc.connection.Factory(
-    wrapper=lambda sock: ssl_context.wrap_socket(sock, server_hostname=IRC_SERVER)
-  )
+  irc3.IrcBot(**config).run()
 
 
-  def on_invite(connection, event):
-    channel = event.arguments[0]
-    if channel == IRC_CHANNEL:
-      print(f"Invited to {IRC_CHANNEL}")
-      connection.join(IRC_CHANNEL)
-    else:
-      print(f"Invited from a non-whitelisted channel: {channel}")
-
-
-  def on_connect(connection, event):
-    print("Connected to server sucessfully")
-    connection.cap('REQ', 'sasl')
-
-  def on_cap(connection, event):
-    print("cap response")
-    if 'ACK' in event.arguments[0] and 'sasl' in event.arguments[0]:
-      connection.send_raw("AUTHENTICATE PLAIN")
-
-  def on_authenticate(connection, event):
-    print("authenticate response")
-    auth_str = f"\0{IRC_USERNAME}\0{IRC_PASSWORD}"
-    b64_auth = base64.b64encode(auth_str.encode()).decode()
-    connection.send_raw(f"AUTHENTICATE {b64_auth}")
-
-  def on_sasl_success(connection, event):
-    connection.cap('END')
-    print("Connected to server sucessfully with sasl")
-    #connection.join("#yourchannel")
-
-  def on_join(connection, event):
-    connection.privmsg(IRC_CHANNEL, "Hello from Python IRC bot!")
-
-  def on_pubmsg(connection, event):
-    user = event.source.nick
-    text = event.arguments[0]
-    print(f"[IRC] <{user}> {text}")
-    message_queue.put(("irc_to_discord", f"<{user}> {text}"))
-
-  def send_from_discord():
-    while True:
-      try:
-        msg_type, msg = message_queue.get(block=True)
-        if msg_type == "discord_to_irc":
-          client.connections[0].privmsg(IRC_CHANNEL, msg)
-      except Exception as e:
-        print("Error sending to IRC:", e)
-
-  try:
-    c = client.server().connect(IRC_SERVER, IRC_PORT, IRC_BOT_NAME,
-          password=IRC_PASSWORD,
-          username=IRC_USERNAME,
-          connect_factory=factory)
-  except irc.client.ServerConnectionError as e:
-    print("Connection failed: ", e)
-    return
-
-  c.add_global_handler("welcome", on_connect)
-  c.add_global_handler("join", on_join)
-  c.add_global_handler("invite", on_invite)
-  c.add_global_handler("pubmsg", on_pubmsg)
-  c.add_global_handler("cap", on_cap)
-  c.add_global_handler("authenticate", on_authenticate)
-  c.add_global_handler("903", on_sasl_success)  # SASL success numeric
-
-  threading.Thread(target=send_from_discord, daemon=True).start()
-
-  client.process_forever()
+#  client = irc.client.Reactor()
+#
+#
+#  def on_pubmsg(connection, event):
+#    user = event.source.nick
+#    text = event.arguments[0]
+#    print(f"[IRC] <{user}> {text}")
+#    message_queue.put(("irc_to_discord", f"<{user}> {text}"))
+#
+#  def send_from_discord():
+#    while True:
+#      try:
+#        msg_type, msg = message_queue.get(block=True)
+#        if msg_type == "discord_to_irc":
+#          client.connections[0].privmsg(IRC_CHANNEL, msg)
+#      except Exception as e:
+#        print("Error sending to IRC:", e)
+#
+#  try:
+#    c = client.server().connect(IRC_SERVER, IRC_PORT, IRC_BOT_NAME,
+#          password=IRC_PASSWORD,
+#          username=IRC_USERNAME,
+#          connect_factory=factory)
+#  except irc.client.ServerConnectionError as e:
+#    print("Connection failed: ", e)
+#    return
+#
+#  c.add_global_handler("welcome", on_connect)
+#  c.add_global_handler("join", on_join)
+#  c.add_global_handler("invite", on_invite)
+#  c.add_global_handler("pubmsg", on_pubmsg)
+#  c.add_global_handler("cap", on_cap)
+#  c.add_global_handler("authenticate", on_authenticate)
+#  c.add_global_handler("903", on_sasl_success)  # SASL success numeric
+#
+#  threading.Thread(target=send_from_discord, daemon=True).start()
+#
+#  client.process_forever()
 
 intents = discord.Intents.default()
 intents.message_content = True
