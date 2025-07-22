6b1281a96a452584729da7ee42d4e62a5b7b5eb6
Stradex <stradexengine@gmail.com>
2025-07-22 12:29:31 +0000

    OK: Use the same only thread for managin messages queues to avoid race condition

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ IRC_USERNAME = os.getenv("IRC_USERNAME")
 IRC_PASSWORD = os.getenv("IRC_PASSWORD")
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.messages = True
+discord_client = discord.Client(intents=intents)
+channel = None
 
 message_queue = queue.Queue()
 
@@ -38,6 +44,20 @@ config = {
   'loop': asyncio.new_event_loop(),
 }
 
+def message_router(bot, message_queue, discord_client):
+  global channel
+  while True:
+    try:
+      msg_type, msg = message_queue.get()
+      print(f"Trying to send {msg_type}: {msg}")
+      if msg_type == "discord_to_irc":
+        bot.privmsg(IRC_CHANNEL, msg)
+      elif msg_type == "irc_to_discord":
+        if channel:
+          asyncio.run_coroutine_threadsafe(channel.send(msg), discord_client.loop)
+    except Exception as e:
+      print("Error in router:", e)
+
 @irc3.plugin
 class IRCDiscordBot:
   def __init__(self, bot):
@@ -59,35 +79,19 @@ class IRCDiscordBot:
 
   @irc3.event(irc3.rfc.PRIVMSG)
   def on_pubmsg(self, mask, event, target, data, **kwargs):
+    global message_queue
     if target.startswith('#'):
       user = mask.nick
       text = data
       print(f"[IRC] <{user}> {text}")
       message_queue.put(("irc_to_discord", f"<{user}> {text}"))
 
-def irc_bot(message_queue):
+def irc_bot(message_queue, discord_client):
   bot = irc3.IrcBot(**config)
 #  bot = irc3.IrcBot.from_config(config)
-
-  def send_from_discord(bot, message_queue):
-    while True:
-      try:
-        msg_type, msg = message_queue.get(block=True)
-        if msg_type == "discord_to_irc":
-          bot.privmsg(IRC_CHANNEL, msg)
-      except Exception as e:
-        print("Error sending to IRC:", e)
-
-
-  threading.Thread(target=send_from_discord, args=(bot, message_queue), daemon=True).start()
+  threading.Thread(target=message_router, args=(bot, message_queue, discord_client), daemon=True).start()
   bot.run(forever=True)
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.guilds = True
-intents.messages = True
-discord_client = discord.Client(intents=intents)
-channel = None
 
 @discord_client.event
 async def on_ready():
@@ -103,26 +107,14 @@ async def on_ready():
 
 @discord_client.event
 async def on_message(message):
+  global message_queue
   if message.author == discord_client.user:
     return
   print(f"[DISCORD] <{message.author}> {message.content}")
   message_queue.put(("discord_to_irc", f"<{message.author}> {message.content}"))
 
-def send_from_irc():
-  global channel
-  while True:
-    try:
-      msg_type, msg = message_queue.get(block=True)
-      if msg_type == "irc_to_discord":
-        if channel:
-          asyncio.run_coroutine_threadsafe(channel.send(msg), discord_client.loop)
-    except Exception as e:
-      print("Error sending to Discord:", e)
-
-
 
 if __name__ == "__main__":
-  irc_thread = threading.Thread(target=irc_bot, args=(message_queue,))
+  irc_thread = threading.Thread(target=irc_bot, args=(message_queue,discord_client))
   irc_thread.start()
-  threading.Thread(target=send_from_irc, daemon=True).start()
   discord_client.run(DISCORD_BOT_TOKEN)
